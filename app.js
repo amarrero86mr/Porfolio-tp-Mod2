5925371e1698157b8ed6d565227cee6f186773c1
amarrero86mr <amarrero86mr@gmail.com>
2023-03-16 13:46:09 -0300

    archivo javascrip

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function cambiarParrafo(){
-document.getElementById()
+function cambiar_parrafo(){
+    document.getElementById("edit-acercade").style.display="block";
+    let texto = document.getElementById("text-acercade").innerText;
+    console.log(texto);
 }
+function myFunction2(valor){
+    document.getElementById("text-acercade").innerText = valor;
+}
+
+document.getElementById("edit-acercade").addEventListener("keydown", function (event) {
+    if (event.key=="Enter"){
+        document.getElementById("edit-acercade").style. display="none";
+    }
+});
